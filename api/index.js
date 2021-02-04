0b7d2be2d5c55e9c252fd2f455fdbf49a6a001da
Paulo Melo <peimelo@gmail.com>
2021-02-04 19:04:14 -0300

    Configura para deploy no Vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const sessionRouters = require('./routes/session.router');
-const heroesRouters = require('./routes/heroes.router');
+const sessionRouters = require('../routes/session.router');
+const heroesRouters = require('../routes/heroes.router');
 
 const app = express();
 
@@ -84,3 +84,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server listening on port %s', port);
 });
+
+module.exports = app;
